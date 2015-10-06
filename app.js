1ae294ef71ae288fc465a2bfca6efc9f5b959661
Marco Morales <marco.bbcm@gmail.com>
2015-10-05 21:36:46 -0700

    Removes 'users' namespace generated by express-generator

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

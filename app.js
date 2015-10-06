8557c6f70fa159273f7a9cbbeb0d8579cadbf496
Marco Morales <marco.bbcm@gmail.com>
2015-10-05 21:26:29 -0700

    Moves api functions to routes/index.js; updates port number
    Moved the api router functions and requests to its own file.
    Added new routes for '/vadio' which routes to the search page and
    '/searching' which routes the ajax request to the server when
    the form is submitted. Added a getVadio() function which handles the
    external API request and returns the response back to the client/ajax.

--- app.js
+++ app.js
@@ -63,83 +63,12 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
-
-
-// Problem: Need video data from Vadio
-// Solution: Use node to access the Vadio Search API
-
-// require the APIs
-var http = require("http");
-var queryString = require("querystring");
-
 // Set up a server
 
 // Setup port
-const PORT = 8080;
-
-// Function which handles requests and responses
-function handleRequest(request, response){
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE 'html'>");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("<h1>Hello World!</h1>");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
-}
-
-// Create a server
-var server = http.createServer(handleRequest);
+const PORT = 4240;
 
 // Start server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.listen(PORT, function(){
+  console.log("I'm listening on port: " + PORT);
 });
-
-// Set up the API call
-
-// Connect to the API url
-var vadioAPI = "api.vadio.com";
-var context = "video";
-
-var vadioOptions = {
-  artist: "Bruno Mars",
-  title: "Uptown Funk",
-  country: "US"
-};
-
-var query = "/v0.9c/" + context + "?" + queryString.stringify(vadioOptions);
-
-var requestOptions = {
-  host: vadioAPI,
-  path: query
-};
-
-// Set up the callback
-var callback = function(response) {
-  var body = "";
-  response.on("data", function(chunk){
-    body += chunk;
-  });
-  response.on("end", function(){
-    console.dir(body);
-  });
-};
-
-// Read the data from the response
-var request = http.get(requestOptions, callback);
-
-// Handle errors from the response
-request.on("error", function(error){
-  console.error(error.message);
-});
-
-// End the request
-request.end();
-
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+        app = express(),
+     router = express.Router(),
+       http = require("http"),
+queryString = require("querystring");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+app.use(router);
+
+// Get vadio API
+
+var vadioAPI = "api.vadio.com",
+     context = "/video",
+  apiVersion = "/v0.9c"
+
+// Requests
+
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/vadio', function(req, res, next) {  
+  res.render('search', {title: 'Search Vadio'});
+});
+
+router.get('/searching', function(req, res){
+  var params = req.query;
+  getVadio(params, function(data){
+    res.send(data);
+  });
+});
+
+// request to vadio API
+
+function getVadio(params, cb) {
+  var query = queryString.stringify(params);
+
+  var vadioOptions = {
+    host: vadioAPI,
+    path: apiVersion + context + "?" + query
+  };
+
+  return http.get(vadioOptions, function(response){
+    var body = "";
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+    
+    response.on('end', function () {
+      var data = JSON.parse(body);
+      console.log(response.req.path);
+      cb(data);
+    });
+
+    response.on('error', function(error){
+      console.error(error.message);
+    });
+  });  
+}
 
 module.exports = router;

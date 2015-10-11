f38ed4c79c877b02ef45f14eca3ee13dcaa1f516
Marco Morales <marco.bbcm@gmail.com>
2015-10-10 23:49:14 -0700

    Refactors and debugs client-side JS; adds comments; combines JS files

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,234 +1,328 @@
-var msgs = {
-  'no-results': 'Sorry, no results at this time. Try another search.',
-  'server-down': 'Sorry, our servers are not responding at this time. Try again later.'
-}
-
-var stagedVideos = [];
-var searchResults = {};
-
+// A collection of videos saved by the user to their playlist
 var playlist = window.localStorage;
-var playlistIDs = Object.keys(playlist);
 
-function objectifyPlaylist() {
-  return playlistIDs.map(function(value) {
-    return playlist.getItem(value);
-  });
-}
-
-var playlistObjs = objectifyPlaylist();
-
-function loadNoResults() {
-  $('#data').html('Sorry, no results found.');
-}
-
-// Returns an array of Video objects
-function makeVideos(vids) {
-  var newVideos = vids.map(function(value) {
-    return new Video(value);
-  });
-  return newVideos;
-}
-
-// Return an array of videoEntries from data
-function videosFromData(data) {
-  var videos = Object.keys(data.videoEntries).map(function(value) {
-    return data.videoEntries[value];
-  });
-  return makeVideos(videos);
-}
-
-function addToStagedVideos(videos) {
-  stagedVideos = videos;
-}
-
-function storeLocally(video) {
-  var dataString = JSON.stringify(video);
-  playlist.setItem(video.videoId, dataString);
-}
-
-function storeSearchResult(video) {
-  var dataString = JSON.stringify(video);
-  searchResults[video.videoId] = dataString;
-}
-
-function addToPlaylist() {
-  for (var i = 0; i < stagedVideos.length; i++) {
-    storeLocally(videos[i]);
-  }
-}
-
-function addToSearchResults(videos) {
-  for (var i = 0; i < videos.length; i++) {
-    storeSearchResult(videos[i]);
-  }
-}
-
-function loadVideosFromAPI(data) {
-  if (data === null) {
-    loadNoResults();
-  } else {
-    var newVideos = videosFromData(data);
-    addToSearchResults(newVideos);
-    addToStagedVideos(newVideos);
-    loadToSearchList(stagedVideos);
-  }
-}
-
-function videosFromLocalStorage() {
-  var videos = playlistObjs.map(function(obj) {
-    return JSON.parse(obj);
-  });
-  stagedVideos = stagedVideos.concat(makeVideos(videos));
-  return stagedVideos;
-}
-
-function getTemplate(type) {
-  // Load search list template
-  var template = document.getElementById(type + '-item');
-  return document.importNode(template.content, true);
-}
-
-function loadVideoTemplate(clone, video) { 
-  clone.querySelector('li').dataset.id = video.id;
-  if (video.artist) {
-    clone.querySelector('.artist').innerText = video.artist;
-  }
-  clone.querySelector('.title').innerText = video.title;
-  var bgUrl = 'url("' + video.image + '")';
-  clone.querySelector('.thumb').style.backgroundImage = bgUrl;
-  return clone;
-}
-
-function getVideoUrl(video) {
-  var videoType = video.videoType;
-  var videoPath;
-  switch (videoType) {
-    case 'yt':
-      videoPath = 'http://www.youtube.com/embed/' + video.videoId + '?autoplay=1'
-  }
-  return videoPath;
-}
-
-function getEmbedTemplate(type) {
-  // Load embedded video template
-  var template;
-  switch (type) {
-    case "yt":
-      template = document.querySelector('#yt-embed');
-  }
-  return document.importNode(template.content, true);
-}
-
-function embedVideo(url) {
-  var player = document.getElementById('player')
-  player.querySelector('iframe').src = url;
-}
-
-function playVideo() {
-  document.getElementById('player').classList.remove('closed');
-  var listId = $(this).closest('ul').attr("id");
-  var listItem = $(this).closest('li');
-  var id = listItem.attr('data-id');
-  var video;
-  if (listId === 'searchlist') {
-    video = JSON.parse(searchResults[id]);
-  } else {
-    video = JSON.parse(playlist[id]);
-  }
-  var url = getVideoUrl(video);
-  embedVideo(url);
-}
-
-function bindPlayEvents(list) {
-  var list = document.getElementById(list);
-  var playButtons = list.querySelectorAll('.play');
-  for(var i = 0; i < playButtons.length; i++) {
-    playButtons[i].addEventListener('click', playVideo, false);
-  }
-}
-
-function loadToSearchList(videos) {
-  var items = [];
-  for (var i = 0; i < videos.length; i++) {
-    var template = getTemplate('searchlist');
-    var data = {
-      'artist': videos[i].artist,
-      'title': videos[i].title,
-      'image': videos[i].preferredThumb().url,
-      'id': videos[i].videoId
-    };
-    var clone = loadVideoTemplate(template, data);
-    items.push(clone);
-  };
-  var searchList = document.getElementById('searchlist');
-  var topResult = searchList.firstChild;
-  for (var i = 0; i < items.length; i++) {
-    searchList.insertBefore(items[i], topResult);
-  }
-  bindPlayEvents('searchlist');
-}
-
-function loadToPlaylist(videos) {
-  var items = [];
-  for (var i = 0; i < videos.length; i++) {
-    var template = getTemplate('playlist');
-    var data = {
-      'artist': videos[i].artist,
-      'title': videos[i].title,
-      'image': videos[i].preferredThumb().url,
-      'id': videos[i].videoId
-    };
-    var clone = loadVideoTemplate(template, data);
-    items.push(clone);
-  };
-  var plist = document.getElementById('playlist');
-  var topResult = plist.firstChild;
-  for (var i = 0; i < items.length; i++) {
-    plist.insertBefore(items[i], topResult);
-  }
-  bindPlayEvents('playlist');
-}
-
-function printErrorMsg(msg) {
-  console.log(msg);
-  var msgBox = document.getElementById('message-box');
-  var span = msgBox.querySelector('span');
-  span.innerText = msg;
-  span.classList.remove('invisible');
-  window.setTimeout(function() {
-    span.classList.add('invisible');
-  }, 5000);
-}
-
-function statusCode(data) {
-  return data.meta.status_code;
-}
 
 $(document).ready(function() {
 
+  // An array of playlist video ids
+  var playlistIDs = Object.keys(playlist);
 
-  // Load the playlist
+
+  var playlistObjs = objectifyPlaylist();
+
+
+  // Preferred image resolution order
+  var resPrefs = ["medium", "high", "default"];
+
+
+  // Messages for the user  
+  var msgs = {
+    'no-results': 'Sorry, no results at this time. Please try another search.',
+    'server-down': 'Sorry, our servers are not responding at this time. Please try again later.'
+  };
+
+
+  // stageVideos holds Video objects ready to be loaded to the DOM
+  var stagedVideos = [];
+
+
+  // searchResults holds JSON of the most recent search data
+  var searchResults = {};
+
+  
+  // Video class
+  function Video(obj) { 
+    this.artist = obj.artist;
+    this.title = obj.title;
+    this.videoId = obj.videoId;
+    this.duration = obj.duration;
+    this.thumbnails = obj.thumbnails;
+    this.videoType = obj.videoType;
+  }
+
+
+  // Video class functions
+  Video.prototype = {
+    preferredThumb: function() {
+      var thumb;
+      var i = 0;
+      while (thumb == undefined) {
+        var res = resPrefs[i];
+        var thumbAtRes = this.findByRes(res);
+        if (thumbAtRes)
+          thumb = thumbAtRes;
+        i++;
+      }
+      return thumb;
+    },
+
+    findByRes: function(res) {
+      return this.thumbnails.filter(function(i) {
+        return i.resolution == res;
+      })[0];
+    },
+
+    inPlaylist: function(playlist) {
+      return playlist.indexOf(this.videoId) >= 0
+    }
+  }
+
+
+  // Gets the template for list items from HTML <head>
+  // and creates a clone
+  function getListTemplate(type) {
+    var template = document.getElementById(type + '-item');
+    return document.importNode(template.content, true);
+  }
+
+
+  // Loads the template with the video data:
+  // id, artist, title, image
+  function loadVideoTemplate(clone, video) { 
+    var bgUrl = 'url("' + video.image + '")';
+    clone.querySelector('li').dataset.id = video.id;
+    clone.querySelector('.artist').innerText = video.artist;
+    clone.querySelector('.title').innerText = video.title;
+    clone.querySelector('.thumb').style.backgroundImage = bgUrl;
+    return clone;
+  }
+
+
+  // Generates a video URL based on the video type
+  function getVideoUrl(video) {
+    var videoPath;
+    switch (video.videoType) {
+      case 'yt':
+        videoPath = 'http://www.youtube.com/embed/' + video.videoId + '?autoplay=1';
+    }
+    return videoPath;
+  }
+
+
+  // Adds the video url to the player iframe in the DOM
+  function embedVideo(url) {
+    var player = document.getElementById('player');
+    player.querySelector('iframe').src = url;
+  }
+
+
+  // Finds the parent list [searchlist, playlist]
+  // of the list item
+  function parentList(item) {
+    return $(item).closest('ul').attr('id');
+  }
+
+
+  // Expands the player 'window'
+  function openPlayerView() {
+    document.getElementById('player').classList.remove('closed');
+  }
+
+
+  // Open the player element and plays the video
+  function playVideo() {
+    var video;
+
+    var listId = parentList(this);
+    var listItem = $(this).closest('li');
+    var id = listItem.attr('data-id');
+
+    openPlayerView();
+    
+    if (listId === 'searchlist') {
+      video = JSON.parse(searchResults[id]);
+    } else {
+      video = JSON.parse(playlist[id]);
+    }
+
+    var url = getVideoUrl(video);
+    embedVideo(url);
+  }
+
+
+  // Binds an event listener to all play buttons
+  // for aspecified list
+  function bindPlayEvents(listView) {
+    var playButtons = listView.querySelectorAll('.play');
+    
+    for(var i = 0; i < playButtons.length; i++) {
+      playButtons[i].addEventListener('click', playVideo, false);
+    }
+  }
+
+
+  // Produces an array video objects similar
+  // to 'playlist' but without keys
+  function objectifyPlaylist() {
+    return playlistIDs.map(function(value) {
+      return playlist.getItem(value);
+    });
+  }
+
+
+  // Stores video data to localStorage
+  function storeLocally(video) {
+    var dataString = JSON.stringify(video);
+    playlist.setItem(video.videoId, dataString);
+  }
+
+
+  // Iterates over stagedVideos and stores
+  // them to localStorage
+  function addToPlaylist() {
+    for (var i = 0; i < stagedVideos.length; i++) {
+      storeLocally(stagedVideos[i]);
+    }
+  }
+
+
+  // Updates stageVideos to include those in localStorage
+  // Returns updated staged videos
+  function videosFromLocalStorage() {
+    var videos = playlistObjs.map(function(obj) {
+      return JSON.parse(obj);
+    });
+    stagedVideos = stagedVideos.concat(makeVideos(videos));
+    return stagedVideos;
+  }
+
+
+  // Instantiates Video objects and 
+  // returns then as an array
+  function makeVideos(vids) {
+    var newVideos = vids.map(function(value) {
+      return new Video(value);
+    });
+    return newVideos;
+  }
+
+
+  // Return an array of videoEntries from data
+  function videosFromData(data) {
+    var videos = Object.keys(data.videoEntries).map(function(value) {
+      return data.videoEntries[value];
+    });
+    return makeVideos(videos);
+  }
+
+
+  // Adds a single search result data 
+  // in searchResults array
+  function storeSearchResult(video) {
+    var dataString = JSON.stringify(video);
+    searchResults[video.videoId] = dataString;
+  }
+
+
+  // Iterates over all search results for 
+  // storing in searchResults array
+  function addToSearchResults(videos) {
+    for (var i = 0; i < videos.length; i++) {
+      storeSearchResult(videos[i]);
+    }
+  }
+
+
+  // Captures the data needed for the view
+  function selectData(video) {
+    return {
+      'artist': video.artist,
+      'title': video.title,
+      'image': video.preferredThumb().url,
+      'id': video.videoId
+    };
+  }
+
+
+  // Loads videos to a list view
+  function loadToList(videos, list) {
+    var items = [];
+    var listView = document.getElementById(list);
+
+    
+    // Build all the results
+    for (var i = 0; i < videos.length; i++) {
+      var template = getListTemplate(list);
+      var data = selectData(videos[i]);
+      var clone = loadVideoTemplate(template, data);
+      items.push(clone);
+    }
+
+    console.log(items);
+
+    // Then load them all at once
+    for (var i = 0; i < items.length; i++) {
+      var topResult = listView.firstChild;
+      console.log(topResult);
+      console.log(items[i]);
+      listView.insertBefore(items[i], topResult);
+    }
+
+    bindPlayEvents(listView);
+  }
+
+
+  // Procedure for converting the API data into
+  // Video objects, staging them, and getting them loaded
+  function loadVideosFromAPI(data) {
+    if (data === null) {
+      printErrorMsg(msgs['no-results']);
+    } else {
+      var newVideos = videosFromData(data);
+      addToSearchResults(newVideos);
+      stagedVideos = newVideos;
+      loadToList(stagedVideos, 'searchlist');
+    }
+  }
+
+
+  // Prints a message to the user for 5 seconds
+  function printErrorMsg(msg) {
+    console.log(msg);
+    var msgBox = document.getElementById('message-box');
+    var span = msgBox.querySelector('span');
+    span.innerText = msg;
+    span.classList.remove('invisible');
+    window.setTimeout(function() {
+      span.classList.add('invisible');
+    }, 5000);
+  }
+
+
+  // Gets the status code of success data
+  // that doesn't yield video results
+  function statusCode(data) {
+    return data.meta.status_code;
+  }
+
+
+  // Loads the playlist into view
   $.ajax({
     url: '/',
+
     success: function() {
       var videos = videosFromLocalStorage();
-      loadToPlaylist(videos);
+      loadToList(videos, 'playlist');
     },
+
     error: function(req, stat, err) {
       console.log('an error has occured');
       console.log(err.message);
     }
-  })
+  });
 
-  // Send a query to the Vadio API
+
+  // Sends a query to the Vadio API and loads
+  // it to the view if it isn't a 504 result
   $('#search-vadio').on('submit', function(e) {
-    console.log('submitting...');
     e.preventDefault();
     var params = $(this).serialize();
+    
     $.ajax({
       dataType: 'json',
       url: '/searching',
       data: params,
+      
       success: function(data) {
         if (data.meta) {
           if (statusCode(data) === 504) {
@@ -238,9 +332,10 @@ $(document).ready(function() {
           loadVideosFromAPI(data);
         }
       },
+
       error: function(req, stat, err) {
-        console.log('an error has occured');
-        console.log(err.message);
+        console.log(msgs['server-down']);
+        printErrorMsg(msgs['server-down']);
       }
     });
   });
